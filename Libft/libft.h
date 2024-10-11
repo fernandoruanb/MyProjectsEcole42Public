2d7889e9052539d0d1835545f22714a911c84f0a
fernandoruanb <fernandosingaporefuture@gmail.com>
2024-10-11 14:09:25 -0300

    Welcome

---
+++ Libft/libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fruan-ba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/11 12:41:02 by fruan-ba          #+#    #+#             */
+/*   Updated: 2024/10/11 12:41:03 by fruan-ba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+
+#endif /* LIBFT_H */
---
+++ README.md
@@ -0,0 +1,2 @@
+Hello everyone! Nice to meet you all. My name is Fernando. I hope that you can find useful codes to study and to improve here.
+One more time, It's a pleasure to stay here helping to enhance each skill that you want. :D
