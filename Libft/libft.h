f45acb3d218d0b2fec9087e304deded67df4fd1d
fernandoruanb <fernandosingaporefuture@gmail.com>
2024-10-12 12:52:11 -0300

    ok

--- Libft/libft.h
+++ Libft/libft.h
@@ -23,8 +23,8 @@ int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
-int		ft_islower(int c);
-int		ft_isupper(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 
---
+++ Libft/test_functions/Libft/functions/test_tolower.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_tolower.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fruan-ba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/12 12:01:56 by fruan-ba          #+#    #+#             */
+/*   Updated: 2024/10/12 12:40:44 by fruan-ba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "../../libft.h"
+
+int	main(void)
+{
+	printf("_______2048 -> True | 0 -> False______\n");
+	printf("Teste 1: Caracter -> %c | Result -> %i\n", 'a', ft_tolower('a'));
+	printf("Teste 2: Caracter -> %c | Result -> %i\n", 'z', ft_tolower('Z'));
+	printf("Teste 3: Caracter -> %c | Result -> %i\n", 'm', ft_tolower('m'));
+	printf("Teste 4: Caracter -> %c | Result -> %i\n", 'G', ft_tolower('G'));
+	printf("Teste 5: Caracter -> %c | Result: %i\n", '1', ft_tolower('1'));
+	printf("Teste 6: Caracter -> %c | Result: %i\n", '9', ft_tolower('9'));
+	printf("Teste 7: Caracter -> %c | Result: %i\n", '!', ft_tolower('!'));
+	printf("Teste 8: Caracter -> %c | Result: %i\n", '@', ft_tolower('@'));
+	printf("Teste 9: Caracter -> %c' ' |Result: %i\n", ' ', ft_tolower(' '));
+	printf("Teste 10: Caracter -> %c | Result: %i\n", '&', ft_tolower('&'));
+	printf("Teste 11: Caracter > %c'\\n' | Result: %i\n", '\n', ft_tolower('\n'));
+	printf("Teste 12: Caracter -> %c | Result: %i\n", '0', ft_tolower('0'));
+	printf("Teste 13: Caracter -> %c | Result: %i\n", '#', ft_tolower('#'));
+	printf("Teste 14: Caracter -> %c | Result: %i\n", ']', ft_tolower(']'));
+	printf("Teste 15: Caracter -> %c | Result: %i\n", '+', ft_tolower('+'));
+	printf("Teste 16: Caracter -> %c | Result: %i\n", '_', ft_tolower('_'));
+	printf("Teste 17: Caracter -> %c | Result: %i\n", '~', ft_tolower('~'));
+	printf("Teste 18: Caracter -> %c | Result: %i\n", '?', ft_tolower('?'));
+	printf("Teste 19: Caracter -> %c | Result: %i\n", ':', ft_tolower(':'));
+	printf("Teste 20: Caracter -> %c | Result: %i\n", '%', ft_tolower('%'));
+	printf("____________FINISHED :D_______________\n");
+	return (0);
+}
