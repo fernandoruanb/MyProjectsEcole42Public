008059f8e88810233147b9b3b150b3cb69b2ddf8
fernandoruanb <fernandosingaporefuture@gmail.com>
2024-10-19 10:11:16 -0300

    ok

--- Libft/libft.h
+++ Libft/libft.h
@@ -55,5 +55,6 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
 t_list	*ft_lstnew(void *content);
+char	**ft_split(char const *s, char c);
 
 #endif /* LIBFT_H */
