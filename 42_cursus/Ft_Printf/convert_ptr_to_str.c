7c3193a27f7d9644298ce5c67d340295aa90feba
fernandoruanb <fernandosingaporefuture@gmail.com>
2024-11-11 17:26:54 -0300

    ok

---
+++ 42_cursus/Ft_Printf/convert_ptr_to_str.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   convert_ptr_to_str.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 11:59:23 by fruan-ba          #+#    #+#             */
+/*   Updated: 2024/11/11 12:21:57 by fruan-ba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static int	calculate_how_many_numbers(unsigned long number);
+
+char	*hex_to_str(unsigned long number);
+
+int	convert_ptr_to_str(va_list args)
+{
+	unsigned long	number;
+	char	*buffer;
+	size_t	length;
+	int	result;
+
+	number = (unsigned long)va_arg(args, void *);
+	buffer = hex_to_str(number);
+	if (!buffer)
+		return (-1);
+	length = ft_strlen(buffer);
+	result = write(1, buffer, length);
+	free(buffer);
+	return (result);
+}
+
+char	*hex_to_str(unsigned long number)
+{
+	char	*buffer;
+	char	*hex_digits;
+	int	length;
+
+	hex_digits = "0123456789abcdef";
+	length = calculate_how_many_numbers(number);
+	buffer = (char *)malloc(length + 1);
+	if (!buffer)
+		return (NULL);
+	buffer[length] = '\0';
+	while (length--)
+	{
+		buffer[length] = (hex_digits[number % 16]);
+		number /= 16;
+	}
+	return (buffer);
+}
+
+static int	calculate_how_many_numbers(unsigned long number)
+{
+	int	length;
+
+	length = 0;
+	if (number == 0)
+		length = 1;
+	while (number)
+	{
+		number /= 16;
+		length++;
+	}
+	return (length);
+}
--- 42_cursus/Ft_Printf/handle_specifier.c
+++ 42_cursus/Ft_Printf/handle_specifier.c
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 13:12:07 by fruan-ba          #+#    #+#             */
-/*   Updated: 2024/11/10 13:39:18 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2024/11/11 14:33:19 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ int	handle_specifier(char specifier, va_list args)
 	else if (specifier == 'u')
 		printed = convert_unsigned_to_str(args);
 	else if (specifier == '%')
-		printed = print_percentage(args);
+		printed = print_percentage();
 	else if (specifier == 'p')
 		printed = print_pointer(args);
 	else
