0e3f41c54e3302d452ca6f18a6f286a3cc854985
fernandoruanb <fernandosingaporefuture@gmail.com>
2024-11-13 14:46:49 -0300

    ok

--- 42_cursus/Ft_Printf/ft_printf.c
+++ 42_cursus/Ft_Printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 13:01:12 by fruan-ba          #+#    #+#             */
-/*   Updated: 2024/11/13 09:55:10 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2024/11/13 14:46:40 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_printf(const char *format, ...)
 	va_list	args;
 	int		printed_char;
 
+	if (!format)
+		return (-1);
 	printed_char = 0;
 	va_start(args, format);
 	printed_char += parse_format(format, args);
