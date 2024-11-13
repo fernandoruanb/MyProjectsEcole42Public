e77fb5ff1b89d25b5e854136a9ce16ec5b4058cb
fernandoruanb <fernandosingaporefuture@gmail.com>
2024-11-13 11:50:47 -0300

    ok

--- 42_cursus/Ft_Printf/backup/handle_specifier.c
+++ 42_cursus/Ft_Printf/backup/handle_specifier.c
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 13:12:07 by fruan-ba          #+#    #+#             */
-/*   Updated: 2024/11/13 11:22:01 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2024/11/13 11:36:50 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,24 @@ int	handle_specifier(char specifier, va_list args, int total)
 
 	printed = 0;
 	if (specifier == 'c')
-		printed += print_char(args);
+		printed = print_char(args);
 	else if (specifier == 's')
-		printed += print_str(args);
+		printed = print_str(args);
 	else if (specifier == 'd' || specifier == 'i')
-		printed += convert_int_to_str(args);
+		printed = convert_int_to_str(args);
 	else if (specifier == 'x' || specifier == 'X')
-		printed += convert_hex_to_str(args, specifier);
+		printed = convert_hex_to_str(args, specifier);
 	else if (specifier == 'u')
-		printed += convert_unsigned_to_str(args);
+		printed = convert_unsigned_to_str(args);
 	else if (specifier == '%')
-		printed += print_percentage();
+		printed = print_percentage();
 	else if (specifier == 'p')
-		printed += convert_ptr_to_str(args);
+		printed = convert_ptr_to_str(args);
 	else
 	{
 		if (total > 0)
 			return (-1);
-		write(1, "%\n", 2);
-		return (-1);
+		write(1, "%", 1);
 	}
 	return (printed);
 }
