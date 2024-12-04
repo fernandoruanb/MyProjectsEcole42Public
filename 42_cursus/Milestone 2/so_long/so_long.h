09b4234bc5ca18c0a905bfb639f110d63affb068
fernandoruanb <fernandosingaporefuture@gmail.com>
2024-12-04 12:17:36 -0300

    upgrade so_long

--- 42_cursus/Milestone 2/so_long/so_long.h
+++ 42_cursus/Milestone 2/so_long/so_long.h
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 15:00:57 by fruan-ba          #+#    #+#             */
-/*   Updated: 2024/12/03 15:16:06 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2024/12/04 12:14:29 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,5 +43,16 @@ int		ft_putstr_fd(const char *s, int fd);
 int		ft_putstr_fd_0(const char *s, int fd);
 int		init_game(t_game *game, char *map_file);
 size_t	ft_strlen(const char *s);
+char	*get_next_line(int fd);
+int	ft_printf(const char *format, ...);
+int	parse_format(const char *format, va_list args);
+int	handle_specifier(va_list args, char specifier);
+int	print_char(va_list args);
+int	print_str(va_list args);
+int	print_percentage(void);
+int	convert_int_to_str(va_list args);
+int	convert_hex_to_str(va_list args, char specifier);
+int	convert_ptr_to_str(va_list args);
+int	convert_unsigned_to_str(va_list args);
 
 #endif /* SO_LONG_H */
