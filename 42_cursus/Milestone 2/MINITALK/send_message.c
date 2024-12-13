fdcab06d954a906511e8bf7b90341c3c33b17f84
fernandoruanb <fernandosingaporefuture@gmail.com>
2024-12-13 15:01:16 -0300

    ok

---
+++ 42_cursus/Milestone 2/MINITALK/send_message.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   send_message.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/13 14:28:47 by fruan-ba          #+#    #+#             */
+/*   Updated: 2024/12/13 14:34:10 by fruan-ba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void	send_message(unsigned char *message)
+{
+	int	index;
+	int	s_index;
+	int	bit;
+
+	index = 0;
+	while (message[index] != '\0')
+	{
+		s_index = 0;
+		while (s_index < 8)
+		{
+			message[index] & 1
+			message[index] << 1
+			s_index++;
+		}
+		index++;
+	}
+}
