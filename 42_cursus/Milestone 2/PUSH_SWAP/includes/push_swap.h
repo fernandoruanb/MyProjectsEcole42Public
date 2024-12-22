0339cfc634804cf453a1c65c7fdcf7bb760ff115
Fernando Ruan <fernandosingaporefuture@gmail.com>
2024-12-21 21:19:07 -0300

    ok

---
+++ 42_cursus/Milestone 2/PUSH_SWAP/includes/push_swap.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/21 21:05:30 by fruan-ba          #+#    #+#             */
+/*   Updated: 2024/12/21 21:05:30 by fruan-ba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdbool.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft/libft.h"
+# include <limits.h>
+
+typedef struct s_node
+{
+	int				value;
+	struct s_node	*next;
+}	t_node;
+
+typedef struct s_stack
+{
+	t_node	*top;
+	int		size;
+}	t_stack;
+
+#endif /* PUSH_SWAP_H */
