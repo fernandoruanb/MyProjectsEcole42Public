80af5df54d2f8c4ee200327d4fa17232c7a8e8ae
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-01-06 17:29:52 -0300

    ok

---
+++ 42_cursus/Milestone 2/PUSH_SWAP/OFFICIAL_LATEST_PUSH/src/push_swap/main.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/22 09:08:01 by fruan-ba          #+#    #+#             */
+/*   Updated: 2025/01/06 10:45:11 by fruan-ba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	int		total_numbers;
+	t_stack	stack;
+
+	if (argc < 2)
+		return (1);
+	total_numbers = count_all_numbers(argc, argv);
+	if (!init_stack(&stack, total_numbers))
+		return (ft_putendl_fd_1("Error", 2));
+	if (!parse_args(argc, argv, &stack))
+		return (ft_putendl_fd_1("Error", 2));
+	if (is_sorted(&stack, stack.size_a, 0))
+	{
+		free(stack.stack_a);
+		free(stack.stack_b);
+		return (0);
+	}
+	free(stack.stack_a);
+	free(stack.stack_b);
+	return (0);
+}
