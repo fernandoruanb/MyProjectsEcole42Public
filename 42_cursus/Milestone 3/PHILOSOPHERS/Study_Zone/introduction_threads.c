a8bf308d079b468ea5aed7e201759cec1fd13c3f
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-01-08 17:03:03 -0300

    ok

---
+++ 42_cursus/Milestone 3/PHILOSOPHERS/Study_Zone/introduction_threads.c
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   introduction_threads.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 16:54:49 by fruan-ba          #+#    #+#             */
+/*   Updated: 2025/01/08 17:02:42 by fruan-ba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h> // Necessária para malloc...
+#include <stdio.h> // Uso de printf
+#include <unistd.h> // Uso de write, read...
+#include <pthread.h> 
