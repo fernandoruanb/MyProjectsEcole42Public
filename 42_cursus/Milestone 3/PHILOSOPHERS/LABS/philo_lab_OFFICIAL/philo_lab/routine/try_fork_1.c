cc9869529216994024bdf6432f42f3c3791d617a
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-01-15 17:19:25 -0300

    ok

---
+++ 42_cursus/Milestone 3/PHILOSOPHERS/LABS/philo_lab_OFFICIAL/philo_lab/routine/try_fork_1.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   try_fork_1.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/15 15:49:23 by fruan-ba          #+#    #+#             */
+/*   Updated: 2025/01/15 16:07:38 by fruan-ba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+void	try_fork_1(t_philo *ph)
+{
+	if (ph->flag->died == 1)
+		return ;
+	printf("%ld Philo %ld is thinking\n", new_time(ph) / 1000, ph->num);
+	if (ph->id == ph->c_ph - 1)
+		pthread_mutex_lock(&ph->forks[(ph->id + 1) % ph->c_ph]);
+	else
+		pthread_mutex_lock(&ph->forks[ph->id % ph->c_ph]);
+	if (ph->flag->died == 1)
+	{
+		if (ph->id == ph->c_ph - 1)
+			pthread_mutex_unlock(&ph->forks[(ph->id + 1) % ph->c_ph]);
+		else
+			pthread_mutex_unlock(&ph->forks[ph->id % ph->c_ph]);
+		return ;
+	}
+	else
+		printf("%ld Philo %ld has taken a fork\n", new_time(ph) / 1000, ph->num);
+}
---
+++ 42_cursus/Milestone 3/PHILOSOPHERS/philo/philo_lab/routine/try_fork_2.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   try_fork_2.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/15 15:50:47 by fruan-ba          #+#    #+#             */
+/*   Updated: 2025/01/15 16:20:20 by fruan-ba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+void	try_fork_2(t_philo *ph)
+{
+	if (ph->id == ph->c_ph - 1)
+		pthread_mutex_lock(&ph->forks[ph->id % ph->c_ph]);
+	else
+		pthread_mutex_lock(&ph->forks[(ph->id + 1) % ph->c_ph]);
+	if (ph->flag->died == 1)
+	{
+		pthread_mutex_unlock(&ph->forks[ph->id % ph->c_ph]);
+		pthread_mutex_unlock(&ph->forks[(ph->id + 1) % ph->c_ph]);
+		return ;
+	}
+	printf("%ld Philo %ld has taken a fork\n", new_time(ph) / 1000, ph->num);
+	pthread_mutex_lock(ph->mutex);
+	ph->tl_meal = get_time(ph);
+	ph->m_eaten++;
+	pthread_mutex_unlock(ph->mutex);
+	if (ph->flag->died == 1)
+	{
+		pthread_mutex_unlock(&ph->forks[ph->id % ph->c_ph]);
+		pthread_mutex_unlock(&ph->forks[(ph->id + 1) % ph->c_ph]);
+		return ;
+	}
+	printf("%ld Philo %ld is eating\n", new_time(ph) / 1000, ph->num);
+	usleep(ph->t_eat * 1000);
+	pthread_mutex_unlock(&ph->forks[ph->id % ph->c_ph]);
+	pthread_mutex_unlock(&ph->forks[(ph->id + 1) % ph->c_ph]);
+}
