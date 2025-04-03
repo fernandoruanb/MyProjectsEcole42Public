b546e47e61ae7e5707e1233dd6b7b6ef070537db
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-04-03 12:51:19 -0300

    ok

--- 42_cursus/Milestone 4/CPP/CPP_00/ex00/megaphone.cpp
+++ 42_cursus/Milestone 4/CPP/CPP_00/ex00/megaphone.cpp
@@ -6,19 +6,18 @@
 /*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 14:24:00 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/03/30 14:43:55 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/04/03 12:44:54 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-using namespace std;
 
 static void	manage_message(char c)
 {
 	if (c >= 'a' && c <= 'z')
-		cout << (char)(c - 32);
+		std::cout << (char)(c - 32);
 	else
-		cout << c;
+		std::cout << c;
 }
 
 int	main(int argc, char **argv)
@@ -38,12 +37,12 @@ int	main(int argc, char **argv)
 				manage_message(argv[index][count]);
 				count++;
 			}
-			cout << " ";
+			std::cout << " ";
 			index++;
 		}
 	}
 	else
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	cout << endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << std::endl;
 	return (0);
 }
