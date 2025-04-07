7c02b9640fc2377912c9dd51c1df9f453464a1ee
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-04-07 17:01:46 -0300

    ok

--- 42_cursus/Milestone 4/CPP/CPP_01/ex04/main.cpp
+++ 42_cursus/Milestone 4/CPP/CPP_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 12:59:00 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/04/07 15:10:55 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/04/07 16:03:57 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,13 @@ int	main(int argc, char **argv)
 	input = argv[1];
 	s1 = argv[2];
 	s2 = argv[3];
-	std::ifstream file(input);
+	/*std::ifstream file(input);
 	if (!file.is_open() || file.peek() == EOF)
 	{
 		std::cout << "The filename needs to be real and with content!!!" << std::endl;
 		return (1);
 	}
-	/*std::ofstream file_out("output.txt");
+	std::ofstream file_out("output.txt");
 	if (!file_out.is_open())
 	{
 		std::cout << "ERROR opening the output file" << std::endl;
