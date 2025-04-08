317ac3b6a6843d777599e05362e7a1b79fff65a9
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-04-08 13:03:18 -0300

    ok

--- 42_cursus/Milestone 4/CPP/CPP_01/ex04/main.cpp
+++ 42_cursus/Milestone 4/CPP/CPP_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 12:59:00 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/04/08 12:42:29 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/04/08 13:02:35 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,14 +47,14 @@ int	main(int argc, char **argv)
 	input = argv[1];
 	s1 = argv[2];
 	s2 = argv[3];
-	std::ifstream file(input);
+	std::ifstream file(input.c_str());
 	if (!file.is_open() || file.peek() == EOF)
 	{
 		std::cerr << "You need to input a valid file, please" << std::endl;
 		return (1);
 	}
 	output = input + ".replace";
-	std::ofstream out(output);
+	std::ofstream out(output.c_str());
 	if (!out.is_open())
 	{
 		std::cerr << "Unfortunately, we failed to open the output file" << std::endl;
