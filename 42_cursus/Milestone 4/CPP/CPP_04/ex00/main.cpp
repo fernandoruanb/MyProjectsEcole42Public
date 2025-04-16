1ede7893d8b0e0c2c01079ee277c2d6dc747d3aa
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-04-16 12:49:08 -0300

    ok

--- 42_cursus/Milestone 4/CPP/CPP_04/ex00/main.cpp
+++ 42_cursus/Milestone 4/CPP/CPP_04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 11:56:05 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/04/16 12:02:29 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/04/16 12:47:28 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,14 @@ int	main(void)
 	WrongCat* e1 = new WrongCat("false cat");
 	WrongCat* e2 = new WrongCat;
 
+	std::cout << std::endl;
 	c1->makeSound();
 	c2->makeSound();
 	d1->makeSound();
 	d2->makeSound();
 	e1->makeSound();
 	e2->makeSound();
+	std::cout << std::endl;
 	delete(c1);
 	delete(c2);
 	delete(d1);
