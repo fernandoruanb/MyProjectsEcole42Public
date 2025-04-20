82594a82d82a0c1673ff1a7cea4d40c104262ed1
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-04-19 21:07:25 -0300

    ok

--- 42_cursus/Milestone 4/Cub3D/srcs/validation/check_true_map.c
+++ 42_cursus/Milestone 4/Cub3D/srcs/validation/check_true_map.c
@@ -6,13 +6,13 @@
 /*   By: fruan-ba <fruan-ba@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 19:39:09 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/04/19 20:46:23 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/04/19 21:06:53 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
-static int	check_newlines(t_game *game)
+static int	check_special_chars(t_game *game)
 {
 	int	index;
 
@@ -21,7 +21,13 @@ static int	check_newlines(t_game *game)
 	{
 		if (game->true_game_map[index]
 			&& (game->true_game_map[index][0] == '\n'
-			|| game->true_game_map[index][0] == '\t'))
+			|| game->true_game_map[index][0] == '\t'
+			|| game->true_game_map[index][0] == '\v'
+			|| game->true_game_map[index][0] == '\f'
+			|| game->true_game_map[index][0] == '\r'
+			|| game->true_game_map[index][0] == '\b'
+			|| game->true_game_map[index][0] == '\a'
+			|| game->true_game_map[index][0] == '\\'))
 			return (0);
 		index++;
 	}
@@ -30,7 +36,7 @@ static int	check_newlines(t_game *game)
 
 int	check_true_map(t_game *game)
 {
-	if (!check_newlines(game))
+	if (!check_special_chars(game))
 		return (ft_putendl_fd_0("Newlines or Tabs found on map", 2));
 	return (1);
 }
