0bd1a9b71a830b9d60a1d000db8f5334a655de44
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-04-20 21:04:18 -0300

    ok

--- 42_cursus/Milestone 4/Cub3D/includes/cub3d.h
+++ 42_cursus/Milestone 4/Cub3D/includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 18:31:34 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/04/20 17:01:55 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/04/20 21:03:36 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ typedef struct s_game
 	char	*ceiling_colours;
 	char	**true_game_map;
 	int		players;
+	int		invalid_map;
 	int		player_x;
 	int		player_y;
 	char	**maps;
@@ -40,23 +41,25 @@ char	**get_map(const char *filename);
 int		free_game(t_game *game);
 void	show_map(t_game *game);
 void	init_all_things(t_game *game);
-int	get_map_textures(t_game *game);
-int	get_no_texture(t_game *game);
-int	get_so_texture(t_game *game);
-int	get_we_texture(t_game *game);
-int	get_ea_texture(t_game *game);
-int	get_floor_colours(t_game *game);
-int	get_ceiling_colours(t_game *game);
-int	get_true_map(t_game *game);
+int		get_map_textures(t_game *game);
+int		get_no_texture(t_game *game);
+int		get_so_texture(t_game *game);
+int		get_we_texture(t_game *game);
+int		get_ea_texture(t_game *game);
+int		get_floor_colours(t_game *game);
+int		get_ceiling_colours(t_game *game);
+int		get_true_map(t_game *game);
 void	show_textures(t_game *game);
-int	is_anything_null(t_game *game);
-int	is_valid_map(t_game *game);
-int	check_colours(t_game *game);
-int	check_true_map(t_game *game);
-int	check_is_valid_png(t_game *game);
-int	check_elements(t_game *game);
-int	check_entire_wall(t_game *game, int index);
-int	check_first_last_wall(t_game *game, int index);
-size_t		get_all_lines(t_game *game);
+int		is_anything_null(t_game *game);
+int		is_valid_map(t_game *game);
+int		check_colours(t_game *game);
+int		check_true_map(t_game *game);
+int		check_is_valid_png(t_game *game);
+int		check_elements(t_game *game);
+int		check_entire_wall(t_game *game, int index);
+int		check_first_last_wall(t_game *game, int index);
+size_t	get_all_lines(t_game *game);
+int		check_invalid_access(t_game *game);
+int		flood_fill(t_game *game);
 
 #endif /* CUB3D_H */
