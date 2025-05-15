da6e3a6f3bdb6371f5758a11efb6a3c97a5f9fe4
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-05-15 15:32:24 -0300

    ok

--- 42_cursus/Milestone 5/CPP/CPP_05/ex03/main.cpp
+++ 42_cursus/Milestone 5/CPP/CPP_05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 13:19:23 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/05/15 13:52:39 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/05/15 15:31:57 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,23 @@ int	main(void)
 		Bureaucrat	C("Jonathan", 1);
 		Intern	crazyMan;
 		AForm*	work;
+		AForm*	mistake;
 		ShrubberyCreationForm test("MyTreeMyLife");
 		RobotomyRequestForm secondtest("Morpheus");
 		PresidentialPardonForm thirdtest("Fernando");
 		//Bureaucrat	C("Leonard", 0);
 		//Bureaucrat	D("Mike", 2000);
 
+		mistake = crazyMan.makeForm("mistake", "mistakeMan");
 		work = crazyMan.makeForm("robotomy request", "crazyMan");
+		if (mistake != NULL)
+			mistake->beSigned(A);
 		work->beSigned(A);
 		A.signForm(*work);
 		A.executeForm(*work);
 		A.executeForm(test);
+		if (mistake != NULL)
+		A.executeForm(*mistake);
 		test.beSigned(B);
 		test.beSigned(A);
 		thirdtest.beSigned(C);
@@ -54,6 +60,8 @@ int	main(void)
 		secondtest.execute(A);
 		thirdtest.execute(C);
 		delete work;
+		if (mistake != NULL)
+			delete mistake;
 		std::cout << A << std::endl;
 		++A;
 		std::cout << A << std::endl;
