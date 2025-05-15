571e508338ce63b488004adb3adb2a9ec206d438
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-05-14 21:55:45 -0300

    ok

--- 42_cursus/Milestone 5/CPP/CPP_05/OFFICIAL/ex02/main.cpp
+++ 42_cursus/Milestone 5/CPP/CPP_05/OFFICIAL/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 13:19:23 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/05/14 21:22:59 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/05/14 21:54:45 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,18 @@ int	main(void)
 {
 	try
 	{
-		Bureaucrat	A("Thiago", 100);
+		Bureaucrat	A("Thiago", 90);
 		Bureaucrat	B("Morpheus", 10);
 		ShrubberyCreationForm test("Form", 100, 80);
 		//Bureaucrat	C("Leonard", 0);
 		//Bureaucrat	D("Mike", 2000);
 
 		test.beSigned(B);
+		test.beSigned(A);
 		B.signForm(test);
+		A.signForm(test);
 		test.execute(B);
+		test.execute(A);
 		std::cout << A << std::endl;
 		++A;
 		std::cout << A << std::endl;
