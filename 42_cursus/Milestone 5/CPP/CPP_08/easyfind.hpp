90af2e978e4eca9917e8fe352bd03e386276882a
Fernando Ruan <fernandosingaporefuture@gmail.com>
2025-05-23 21:08:24 -0300

    ok

---
+++ 42_cursus/Milestone 5/CPP/CPP_08/easyfind.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/23 20:35:29 by fruan-ba          #+#    #+#             */
+/*   Updated: 2025/05/23 20:35:29 by fruan-ba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <iostream>
+
+template<typename T>
+typename T::iterator easyfind(T& container, int value)
+{
+	while (container.begin())
+}
+
+#endif /* EASYFIND_HPP */
