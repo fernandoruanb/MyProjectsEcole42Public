0253482dfd852900b663b22e7bd80fb74d3b75a8
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-05-24 11:04:53 -0300

    ok

--- 42_cursus/Milestone 5/CPP/CPP_07/ex00/main.cpp
+++ 42_cursus/Milestone 5/CPP/CPP_07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 12:10:37 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/05/21 18:42:19 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/05/24 11:02:23 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@ int	main(void)
 	int	b = 3;
 
 	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << RED "a = " RESET << YELLOW << a << RESET << RED ", b = " RESET << YELLOW << b << RESET << std::endl;
+	std::cout << BRIGHT_MAGENTA "min( a, b ) = " RESET << YELLOW << ::min( a, b ) << RESET << std::endl;
+	std::cout << BRIGHT_MAGENTA "max( a, b ) = " RESET << YELLOW << ::max( a, b ) << RESET << std::endl;
 
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 
 	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << RED "c = " RESET << YELLOW << c << RESET << RED ", d = " RESET << YELLOW << d << RESET << std::endl;
+	std::cout << BRIGHT_MAGENTA "min( c, d ) = " RESET << YELLOW << ::min( c, d ) << RESET << std::endl;
+	std::cout << BRIGHT_MAGENTA "max( c, d ) = " RESET << YELLOW << ::max( c, d ) << RESET << std::endl;
 	return (0);
 }
