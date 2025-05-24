4374508ffebf8a966a2af93b89eb0ef3e6cdd392
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-05-24 20:36:05 -0300

    ok

--- 42_cursus/Milestone 5/CPP/CPP_08/ex00/main.cpp
+++ 42_cursus/Milestone 5/CPP/CPP_08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/24 19:16:25 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/05/24 19:34:22 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/05/24 20:35:24 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int	main(void)
 	int	result;
 	int	value;
 
+	std::cout << std::endl;
+	std::cout << LIGHT_BLUE << std::string(7, ' ') << "AMAZING CPP 07 TESTER =D" RESET << std::endl;
 	try
 	{
 		values.push_back(1);
@@ -26,13 +28,16 @@ int	main(void)
 		value = 2;
 		result = easyfind(values, value);
 		std::cout << BRIGHT_MAGENTA << std::string(40, '=') << RESET << std::endl;
-		std::cout << BRIGHT_GREEN "We found the value: " RESET << BRIGHT_YELLOW << value << RESET << std::endl;
+		std::cout << BRIGHT_GREEN << std::string(10, ' ') <<  "Value found: " RESET << BRIGHT_YELLOW << value << RESET << std::endl;
 		std::cout << BRIGHT_MAGENTA << std::string(40, '=') << RESET << std::endl;
+		value = 10;
+		result = easyfind(values, value);
 	}
 	catch (std::exception &exception)
 	{
+		std::cerr << std::endl;
 		std::cerr << BRIGHT_MAGENTA << std::string(40, '=') << RESET << std::endl;
-		std::cerr << BRIGHT_RED "We didn't find the value D=" RESET << std::endl;
+		std::cerr << BRIGHT_RED << std::string(10, ' ') << "Value not found D= " << YELLOW << value << RESET << std::endl;
 		std::cerr << BRIGHT_MAGENTA <<  std::string(40, '=') << RESET << std::endl;
 	}
 	return (0);
