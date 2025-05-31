4e176a3b4885eec8af0e6606e4558bb7471130a8
Fernando Ruan <fernandosingaporefuture@gmail.com>
2025-05-31 12:44:31 -0300

    ok

--- 42_cursus/Milestone 5/CPP/CPP_09/Labs/lab2.cpp
+++ 42_cursus/Milestone 5/CPP/CPP_09/Labs/lab2.cpp
@@ -1,30 +1,51 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lab1.cpp                                           :+:      :+:    :+:   */
+/*   lab2.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/31 12:07:25 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/05/31 12:07:25 by fruan-ba         ###   ########.fr       */
+/*   Created: 2025/05/31 12:15:29 by fruan-ba          #+#    #+#             */
+/*   Updated: 2025/05/31 12:15:29 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
- 
+
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <map>
 
-int main(void)
+int	main(void)
 {
 	std::ifstream file("data.csv");
-	std::string	line;
+	std::string line;
+	std::map<std::string,double> btc;
+	std::string date;
+	std::string value;
+	double btc_value;
+	size_t	comma;
 
 	if (file.is_open())
 	{
 		while (std::getline(file, line))
 		{
-			std::cout << line << std::endl;
+			comma = line.find(',');
+			if (comma != std::string::npos)
+			{
+				date = line.substr(0, comma);
+				value = line.substr(comma + 1);
+				std::stringstream ss(value);
+				ss >> btc_value;
+				btc[date] = btc_value;
+			}	
 		}
 		file.close();
+		std::map<std::string,double>::iterator it = btc.begin();
+		while (it != btc.end())
+		{
+			std::cout << it->first << ", " << it->second << std::endl;
+			++it;
+		}
 	}
 	return (0);
 }
