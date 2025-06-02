c99870fc242be7dbac54a527b7d93a14bbaa4bc4
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-06-02 18:55:48 -0300

    ok

--- 42_cursus/Milestone 5/CPP/CPP_09/ex02/main.cpp
+++ 42_cursus/Milestone 5/CPP/CPP_09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 16:00:59 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/06/02 18:23:49 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/06/02 18:51:26 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,17 @@ int	main(int argc, char **argv)
 	long long	index;
 	long long	count;
 	long long	num;
+	bool	flag;
+	long long	checker;
 	std::list<int>	pmergeMe;
 
 	index = 0;
 	count = 1;
 	while (count < argc)
 	{
+		flag = 0;
 		index = 0;
+		checker = 0;
 		while (argv[count][index] != '\0')
 		{
 			if (argv[count][index] == ' ')
@@ -40,7 +44,12 @@ int	main(int argc, char **argv)
 		
 			if (argv[count][index] >= '0' && argv[count][index] <= '9')
 			{
-				std::string	token(1, argv[count][index]);
+				while (argv[count][index + checker] && argv[count][index + checker] >= '0'
+						&& argv[count][index + checker] <= '9')
+					checker++;
+				if (checker > 1)
+					flag = 1;
+				std::string	token(argv[count], checker);
 				std::stringstream ss(token);
 				ss >> num;
 				pmergeMe.push_back(num);
@@ -50,9 +59,18 @@ int	main(int argc, char **argv)
 				std::cerr << RED "Error: invalid input detected => " << BRIGHT_YELLOW << argv[count][index] << RESET << std::endl;
 				return (1);
 			}
-			++index;
+			if (flag == 1)
+				index += checker;
+			else
+				++index;
 		}
 		count++;
+	}
+	std::list<int>::const_iterator it = pmergeMe.begin();
+	while (it != pmergeMe.end())
+	{
+		std::cout << *it << std::endl;
+		++it;
 	}
 	return (0);
 }
