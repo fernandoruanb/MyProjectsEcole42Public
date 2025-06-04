8a076712238846d9e21c90cb6809353173a02581
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-06-04 15:21:57 -0300

    ok

--- 42_cursus/Milestone 5/CPP/CPP_09/ex02/main.cpp
+++ 42_cursus/Milestone 5/CPP/CPP_09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 16:00:59 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/06/04 14:03:40 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/06/04 15:19:56 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,11 +76,8 @@ int	main(int argc, char **argv)
 		checker = 0;
 		while (argv[count][index] != '\0')
 		{
-			if (argv[count][index] == ' ')
-			{
+			while (argv[count][index] && argv[count][index] == ' ')
 				++index;
-				continue ;
-			}	
 			if (argv[count][index] >= '0' && argv[count][index] <= '9')
 			{
 				while (argv[count][index + checker] && argv[count][index + checker] >= '0'
@@ -91,6 +88,11 @@ int	main(int argc, char **argv)
 				std::string	token(argv[count], checker);
 				std::stringstream ss(token);
 				ss >> num;
+				if (num < 0 || num > INT_MAX)
+				{
+					std::cerr << RED "Error: overflow/underflow detected" RESET << std::endl;
+					return (1);
+				}
 				pmergeMe.push_back(num);
 				pmergeMe2.push_back(num);
 			}
@@ -106,6 +108,11 @@ int	main(int argc, char **argv)
 		}
 		count++;
 	}
+	if (pmergeMe.size() == 0 || pmergeMe2.size() == 0)
+	{
+		std::cerr << RED "Error: empty values" RESET << std::endl;
+		return (1);
+	}
 	std::cout << MAGENTA "Before LIST: " RESET;
 
 	// I am showing to you all values into LIST here
@@ -117,7 +124,7 @@ int	main(int argc, char **argv)
 	}
 	// I am showing to you all values into VECTOR here
 	std::vector<int>::iterator itv = pmergeMe2.begin();
-	std::cout << std::endl;
+	std::cout << std::endl << std::endl;
 	std::cout << MAGENTA "Before VECTOR: " RESET;
 	while (itv != pmergeMe2.end())
 	{
@@ -126,7 +133,7 @@ int	main(int argc, char **argv)
 	}
 
 	// Start Ford-Johnson algorithm and calculate clocks
-	std::cout << std::endl;
+	std::cout << std::endl << std::endl;
 	startTime = std::clock();
 	fordJohnsonList(pmergeMe);
 	endTime = std::clock();
@@ -144,7 +151,7 @@ int	main(int argc, char **argv)
                 std::cout << BRIGHT_YELLOW << *it << " " << RESET;
                 ++it;
         }
-	std::cout << std::endl;
+	std::cout << std::endl << std::endl;
 	// I am here showing the result of VECTOR
 	std::cout << MAGENTA "After VECTOR: " RESET;
 	itv = pmergeMe2.begin();
@@ -153,7 +160,7 @@ int	main(int argc, char **argv)
 		std::cout << BRIGHT_YELLOW << *itv << " " << RESET;
 		++itv;
 	}
-        std::cout << std::endl;
+        std::cout << std::endl << std::endl;
 
 	// Check the work!!!
 	if (checkListSort(pmergeMe))
