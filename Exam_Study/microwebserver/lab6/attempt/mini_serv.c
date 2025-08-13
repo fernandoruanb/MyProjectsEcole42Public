041334ed79d12407a556ffa966ddb6f67fc5ff49
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-08-13 20:46:26 -0300

    ok

--- Exam_Study/microwebserver/lab6/attempt/mini_serv.c
+++ Exam_Study/microwebserver/lab6/attempt/mini_serv.c
@@ -5,49 +5,75 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: fruan-ba <fruan-ba@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/12 10:12:41 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/08/13 15:15:34 by fruan-ba         ###   ########.fr       */
+/*   Created: 2025/08/13 17:48:59 by fruan-ba          #+#    #+#             */
+/*   Updated: 2025/08/13 20:43:34 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/socket.h>
 #include <stdbool.h>
+#include <sys/socket.h>
 #include <sys/select.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
-typedef struct	s_client
+typedef	struct	s_client
 {
 	char	buffer[90000];
+	size_t	sent_total;
+	size_t	total;
 }	t_client;
 
 typedef struct	s_server
 {
 	int	serverFD;
-	int	port;
-	struct	sockaddr_in	addr;
 	int	clients[FD_SETSIZE];
+	int	port;
 	int	next_id;
 	int	fd_max;
-	char	buffer[90000];
+	char	buffer[10000];
+	struct sockaddr_in	addr;
 }	t_server;
 
 static t_client	clientsBuffer[FD_SETSIZE];
 
 static void	initAllClients(t_server *myServer)
 {
-	size_t	index;
-
-	index = 0;
+	size_t	index = 0;
+	
 	while (index < FD_SETSIZE)
 	{
 		myServer->clients[index] = -1;
 		++index;
 	}
+}
+
+static void	clearBuffer(t_server *myServer)
+{
+	size_t	index = 0;
+
+	while (index < FD_SETSIZE)
+	{
+		myServer->buffer[index] = '\0';
+		++index;
+	}
+}
+
+static void	findNewMax(t_server *myServer, fd_set *active_fds)
+{
+	int	fd = 0;
+	int	fd_max = myServer->serverFD;
+	
+	while (fd <= myServer->fd_max)
+	{
+		if (FD_ISSET(fd, active_fds) && fd > fd_max)
+				fd_max = fd;
+		++fd;
+	}
+	myServer->fd_max = fd_max;
 }
 
 static bool	checkPort(const char *nptr)
@@ -61,56 +87,15 @@ static bool	checkPort(const char *nptr)
 	return (true);
 }
 
-static bool	checkNewLine(t_server *myServer)
-{
-	size_t	index = 0;
-	while (myServer->buffer[index])
-	{
-		if (myServer->buffer[index] == '\n')
-			return (true);
-		++index;
-	}
-	return (false);
-}
-
-static void	clearBuffer(t_server *myServer)
-{
-	size_t	index;
-	size_t	fd = 0;
-
-	index = 0;
-	while (index < sizeof(myServer->buffer))
-	{
-		myServer->buffer[index] = '\0';
-		++index;
-	}
-}
-
-static void	findNewMax(t_server *myServer, fd_set *active_fds)
-{
-	int	fd;
-	int	fd_max = myServer->serverFD;
-
-	fd = 0;
-	while (fd < FD_SETSIZE)
-	{
-		if (FD_ISSET(fd, active_fds) && fd > fd_max)
-			fd_max = fd;
-		++fd;
-	}
-	myServer->fd_max = fd_max;
-}
-
 static int	ft_atoi(const char *nptr, bool *err)
 {
-	int	result;
+	int	result = 0;
 
 	if (*nptr == '\0' || !checkPort(nptr))
 	{
 		*err = true;
-		return (0);
+		return (0);	
 	}
-	result = 0;
 	while (*nptr >= '0' && *nptr <= '9')
 	{
 		result *= 10;
@@ -125,39 +110,13 @@ static int	ft_atoi(const char *nptr, bool *err)
 	return (result);
 }
 
-static void	connectServer(t_server *myServer)
-{
-	char	err[] = "Fatal error\n";
-
-	myServer->serverFD = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (myServer->serverFD == -1)
-	{
-		write(2, err, strlen(err));
-		exit(1);
-	}
-	myServer->addr.sin_family = AF_INET;
-	myServer->addr.sin_port = (myServer->port >> 8) | ((myServer->port & 0xFF) << 8);
-	myServer->addr.sin_addr.s_addr = (1 << 24) | (0 << 16) | (0 << 8) | 127;
-
-	if (bind(myServer->serverFD, (struct sockaddr*)&myServer->addr, sizeof(myServer->addr)) < 0)
-	{
-		write(2, err, strlen(err));
-		close(myServer->serverFD);
-		exit(1);
-	}
-	if (listen(myServer->serverFD, FD_SETSIZE) < 0)
-	{
-		write(2, err, strlen(err));
-		close(myServer->serverFD);
-		exit(1);
-	}
-}
-
 static void	broadcast(int clientFD, t_server *myServer, fd_set *write_fds, int flag)
 {
-	char	msg[90000];
-	size_t	index = 0;
+	char	msg[10000];
+	size_t	index;
+	int	fd;
 
+	index = 0;
 	while (index < sizeof(msg))
 	{
 		msg[index] = '\0';
@@ -166,79 +125,77 @@ static void	broadcast(int clientFD, t_server *myServer, fd_set *write_fds, int f
 	if (flag == 0)
 	{
 		sprintf(msg, "server: client %d just arrived\n", myServer->clients[clientFD]);
-		int fd = 0;
+		fd = 0;
 		while (fd <= myServer->fd_max)
 		{
 			if (FD_ISSET(fd, write_fds) && fd != myServer->serverFD && fd != clientFD)
-				send(fd, &msg, strlen(msg), 0);
+				send(fd, msg, strlen(msg), 0);
 			++fd;
-		}
+		}	
 	}
 	else if (flag == 1)
 	{
 		sprintf(msg, "server: client %d just left\n", myServer->clients[clientFD]);
-		int fd = 0;
+		fd = 0;
 		while (fd <= myServer->fd_max)
 		{
 			if (FD_ISSET(fd, write_fds) && fd != myServer->serverFD && fd != clientFD)
-				send(fd, &msg, strlen(msg), 0);
+				send(fd, msg, strlen(msg), 0);
 			++fd;
 		}
 	}
-	else
+	else if (flag == 2)
 	{
-		ssize_t	len;
+		char	*ptr = myServer->buffer;
 		sprintf(msg, "client %d: ", myServer->clients[clientFD]);
 		strcat(msg, clientsBuffer[clientFD].buffer);
-		char	*ptr = myServer->buffer;
 		while (*ptr)
 		{
-			char temp;
+			char	temp;
 			if (*ptr == '\0')
 				temp = '\0';
 			else
 				temp = *(ptr + 1);
-			if (*ptr == '\n')
+			if ((*ptr == '\\' && temp == 'n') || *ptr == '\n')
 			{
-				ssize_t	total = strlen(msg);
-				ssize_t	sent_total = 0;
-				int	fd = 0;
+				fd = 0;
 				while (fd <= myServer->fd_max)
 				{
 					if (FD_ISSET(fd, write_fds) && fd != myServer->serverFD && fd != clientFD)
 					{
-						sent_total = send(fd, msg, total, 0);
-						if (sent_total == -1)
-							continue ;
-						while (sent_total < total)
-						{
-							ssize_t	tmp;
-							tmp = send(fd, msg + sent_total, total - sent_total, 0);
-							if (tmp == -1)
-								continue ;
-							sent_total += tmp;
-						}
+						send(fd, msg, strlen(msg), 0); 
 						send(fd, "\n", 1, 0);
 					}
 					++fd;
 				}
-				size_t	index = 0;
-				while (index < sizeof(msg))
+				if (*ptr == '\\' && temp == 'n')
 				{
-					msg[index] = '\0';
-					++index;
+					sprintf(msg, "client %d: ", myServer->clients[clientFD]);
+					++ptr;
 				}
-				sprintf(msg, "client %d: ", myServer->clients[clientFD]);
 			}
 			else
 			{
-				char tmp[2] = {*ptr, '\0'};
+				char	tmp[2] = {*ptr, '\0'};
 				strcat(msg, tmp);
 			}
 			++ptr;
 		}
-		bzero(&clientsBuffer[clientFD], sizeof(clientsBuffer[clientFD]));
+		bzero(&clientsBuffer[clientFD].buffer, sizeof(clientsBuffer[clientFD].buffer));
 	}
+}
+
+static bool	checkNewLine(t_server *myServer)
+{
+	size_t	index = 0;
+	
+	while (index < strlen(myServer->buffer))
+	{
+		if (myServer->buffer[index] == '\n')
+			return (true);
+		++index;
+	}
+	return (false);
 }
 
 static void	startWebService(t_server *myServer)
@@ -251,7 +208,6 @@ static void	startWebService(t_server *myServer)
 	FD_ZERO(&active_fds);
 	FD_SET(myServer->serverFD, &active_fds);
 	myServer->fd_max = myServer->serverFD;
-
 	while (1)
 	{
 		fd = 0;
@@ -260,12 +216,13 @@ static void	startWebService(t_server *myServer)
 		if (select(myServer->fd_max + 1, &read_fds, &write_fds, NULL, NULL) < 0)
 		{
 			fd = 0;
-			while (fd <= myServer->fd_max)
+			while (fd <= myServer->serverFD)
 			{
 				if (FD_ISSET(fd, &active_fds))
 					close(fd);
 				++fd;
 			}
+			exit(1);
 		}
 		while (fd <= myServer->fd_max)
 		{
@@ -273,15 +230,15 @@ static void	startWebService(t_server *myServer)
 			{
 				if (fd == myServer->serverFD)
 				{
-					int clientFD = accept(myServer->serverFD, NULL, NULL);
+					int	clientFD = accept(myServer->serverFD, NULL, NULL);
 					if (clientFD > 0)
 					{
-						myServer->clients[clientFD] = myServer->next_id;
-						myServer->next_id++;
+						FD_SET(clientFD, &active_fds);
 						if (clientFD > myServer->fd_max)
 							myServer->fd_max = clientFD;
-						FD_SET(clientFD, &active_fds);
-						broadcast(clientFD, myServer, &write_fds, 0);
+						myServer->clients[clientFD] = myServer->next_id;
+						myServer->next_id++;
+						broadcast(clientFD, myServer, &write_fds, 0); 
 					}
 					else
 					{
@@ -305,16 +262,54 @@ static void	startWebService(t_server *myServer)
 					}
 					else
 					{
-						close(fd);
 						FD_CLR(fd, &active_fds);
-						findNewMax(myServer, &active_fds);
+						close(fd);
 						broadcast(fd, myServer, &write_fds, 1);
+						findNewMax(myServer, &active_fds);
 						bzero(&clientsBuffer[fd].buffer, sizeof(clientsBuffer[fd].buffer));
 					}
 				}
 			}
 			++fd;
 		}
+	}
+}
+
+static void	connectServer(t_server *myServer)
+{
+	char	err[] = "Fatal error\n";
+
+	myServer->serverFD = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (myServer->serverFD == -1)
+	{
+		write(2, err, strlen(err));
+		exit(1);
+	}
+	myServer->addr.sin_family = AF_INET;
+	myServer->addr.sin_port = (myServer->port >> 8) | ((myServer->port & 0xFF) << 8);
+	myServer->addr.sin_addr.s_addr = (1 << 24) | (0 << 16) | (0 << 8) | 127;
+	if (bind(myServer->serverFD, (struct sockaddr*)&myServer->addr, sizeof(myServer->addr)) < 0)
+	{
+		write(2, err, strlen(err));
+		close(myServer->serverFD);
+		exit(1);
+	}
+	if (listen(myServer->serverFD, FD_SETSIZE) < 0)
+	{
+		write(2, err, strlen(err));
+		close(myServer->serverFD);
+		exit(1);
+	}
+}
+
+static void	initAllClientsBuffer(void)
+{
+	size_t	index = 0;
+	
+	while (index < FD_SETSIZE)
+	{
+		bzero(&clientsBuffer[index].buffer, sizeof(clientsBuffer[index].buffer));
+		++index;
 	}
 }
 
@@ -326,18 +321,19 @@ int	main(int argc, char **argv)
 	if (argc != 2)
 	{
 		char	err[] = "Wrong number of arguments\n";
-		write(2, err, strlen(err));
+		write(2, &err, strlen(err));
 		exit(1);
 	}
 	err = false;
 	myServer.port = ft_atoi(argv[1], &err);
 	if (err)
 	{
-		char err[] = "Fatal error\n";
-		write(2, err, strlen(err));
+		char	err[] = "Fatal error\n";
+		write(2, &err, strlen(err));
 		exit(1);
 	}
 	initAllClients(&myServer);
+	initAllClientsBuffer();
 	myServer.next_id = 0;
 	connectServer(&myServer);
 	startWebService(&myServer);
